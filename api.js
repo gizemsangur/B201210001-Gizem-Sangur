989788ddd7667b12d21763390c91e8784b37f76a
gizemsangur <103527969+gizemsangur@users.noreply.github.com>
2022-05-14 20:21:59 +0300

    Add files via upload

--- api.js
+++ api.js
@@ -84,6 +84,7 @@ function apicall(){
 
 		
 	});
+
 }
 
 apicall();
--- iletisim.php
+++ iletisim.php
@@ -1,8 +1,10 @@
 <?php 
-  if(isset($_POST['namesurname'])&&isset($_POST['mail'])&&isset($_POST['message'])){		
+  if(isset($_POST['namesurname'])&&isset($_POST['mail'])&&isset($_POST['message'])&&isset($_POST['fav_language'])&&isset($_POST['contact_reason'])){		
 		$mail=$_POST['mail'];
 		$name=$_POST['namesurname'];
 		$msg=$_POST['message'];
+    $fav_lang=$_POST['fav_language'];
+    $contact=$_POST['contact_reason'];
 		
 		$myfile = fopen("formcontact.txt", "a") or die("Unable to open file!");
     fwrite($myfile,$name );
@@ -10,6 +12,10 @@
     fwrite($myfile,$mail);
     fwrite($myfile,',');
     fwrite($myfile,$msg);
+    fwrite($myfile,',');
+    fwrite($myfile,$fav_lang);
+    fwrite($myfile,',');
+    fwrite($myfile,$contact);
     fwrite($myfile,' / ');
     fclose($myfile);
     header("Location:iletisim.html");
